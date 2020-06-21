d7851b8df5597fd2304345084d4bf91c709a6d90
Karthick M <karthick.m@otr-ict.chn>
2020-06-21 19:52:07 +0530

    pushing to eclipse

--- src/test/java/org/proj/Program1.java
+++ src/test/java/org/proj/Program1.java
@@ -1,7 +1,13 @@
 package org.proj;
 
 public class Program1 {
-public static void main(String[] args) {
+
+	public static void main(String[] args) {
+	
 	System.out.println("vidhya");
-}
+	
+	System.out.println("EMPLOYEE1");
+
+	}
+
 }
